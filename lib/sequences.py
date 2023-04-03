c8ed217626237b158b1577f05f97f85b6c9a2ccb
Nick Gastis <nickgastis1@gmail.com>
2023-04-03 11:40:08 -0400

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+#     print(fibonacci[0: int(length)])
+
+
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    fibonacci_array = [0, 1]
+
+    if length == 1:
+            print([0])
+    elif length == 2:
+        print(fibonacci_array)
+    else:
+        for i in range(2, length):
+                next_num = fibonacci_array[-1] + fibonacci_array[-2]
+                fibonacci_array.append(next_num)
+        print(fibonacci_array)
